2437a1742156a81f12bb644d2cba2419081efa71
Aditya Bhattacharya <Aditya.Bhattacharya@mea.ey.net>
2018-01-03 16:57:30 +0530

    code changes

--- app/shared-components/tomato-chart/tomato-chart.directive.js
+++ app/shared-components/tomato-chart/tomato-chart.directive.js
@@ -1,39 +1,44 @@
-(function() {
+(function  ()  {
     'use strict';
- 
+
     angular
         .module('app')
-        .directive('tomatoChart', tomatoChart);
- 
-    function tomatoChart() {
-        var directive = {
-            scope: {
-                data: "<",
-                type: "@",
-                header: "@?",
-                horizontalAxisLabel: "@?",
-                verticalAxisLabel: "@?",
-                displayLegend: "<?"
+        .directive('tomatoChart',  tomatoChart);
+
+    function  tomatoChart()  {
+        var  directive  =  {
+            scope:  {
+                data:  "<",
+                type:  "@",
+                header:  "@?",
+                horizontalAxisLabel:  "@?",
+                verticalAxisLabel:  "@?",
+                displayLegend:  "<?",
+                id:  "@",
+                className:  "@"
             },
-            restrict: 'E',
-            controller: TomatoChartController,
-            bindToController: true,
-            controllerAs: 'vm',
-            templateUrl: './app/shared-components/tomato-chart/tomato-chart.directive.html'
+            restrict:  'E',
+            controller:  TomatoChartController,
+            bindToController:  true,
+            controllerAs:  'vm',
+            templateUrl:  './app/shared-components/tomato-chart/tomato-chart.directive.html'
         };
- 
-        return directive;
+
+        return  directive;
     }
- 
-    TomatoChartController.$inject = ['tomatoChartService'];
- 
-    function TomatoChartController(tomatoChartService) {
-        var vm = this;
-        vm.chartData = tomatoChartService.getBarChartData(vm.data);
-        vm.chartOptions = tomatoChartService.getChartOptions(
-            vm.horizontalAxisLabel, vm.verticalAxisLabel, 
+
+    TomatoChartController.$inject  =  ['tomatoChartService'];
+
+    function  TomatoChartController(tomatoChartService)  {
+        var  vm  =  this;
+        if  (vm.type  ==  "Bar")  {
+            vm.chartData  =  tomatoChartService.getBarChartData(vm.data);
+        }  else  if  (vm.type  ==  "Line")  {
+            vm.chartData  =  tomatoChartService.getLineChartData(vm.data);
+        }
+        vm.chartOptions  =  tomatoChartService.getChartOptions(
+            vm.horizontalAxisLabel,  vm.verticalAxisLabel,
             vm.displayLegend
-        );
+        )
     }
- })();
- 
+})(); 
